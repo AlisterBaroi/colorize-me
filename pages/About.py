f94bdc10c76703e6872ddc2ad173a2985a9015dc
Alister Baroi <alister.baroi@gmail.com>
2025-05-29 20:50:43 +0100

    about init

--- pages/About.py
+++ pages/About.py
@@ -10,5 +10,44 @@ st.set_page_config(
 )
 
 
-st.header("Colorize Me")
-st.subheader("Author: Alister Baroi")
+st.title("*White Paper —*")
+text = """**:green[Author:]**  *Alister Animesh Baroi* \\
+**:green[GitHub Repo:]**  *[https://github.com/AlisterBaroi/colorize-me](https://github.com/AlisterBaroi/colorize-me)*
+"""
+st.write(text)
+st.divider()
+
+# Abstract
+st.subheader("*:blue[Abstract —]*")
+st.write(
+    "This website is build by me :red[(Alister Baroi / Alister Animesh Baroi)], as my online resume, & as my porfolio website, to showcase coding skills & projects."
+)
+
+
+# st.divider()
+st.subheader("*:blue[Introduction —]*")
+st.write(
+    "This website is build by me :red[(Alister Baroi / Alister Animesh Baroi)], as my online resume, & as my porfolio website, to showcase coding skills & projects."
+)
+st.write(
+    "This website is made with web app behavior and user interactivity in mind, thus focusing on the :green[User Interface] & :green[User Experience (UI/UX)] for elivated engagement."
+)
+
+# st.divider()
+st.subheader("*:blue[Methodology —]*")
+st.write(
+    "This website is build by me :red[(Alister Baroi / Alister Animesh Baroi)], as my online resume, & as my porfolio website, to showcase coding skills & projects."
+)
+st.write(
+    "This website is made with web app behavior and user interactivity in mind, thus focusing on the :green[User Interface] & :green[User Experience (UI/UX)] for elivated engagement."
+)
+
+# st.divider()
+st.subheader("*:blue[References —]*")
+st.write(
+    "[1] This website is build by me :red[(Alister Baroi / Alister Animesh Baroi)], as my online resume, & as my porfolio website, to showcase coding skills & projects."
+    "[2] asdsad"
+)
+st.write(
+    "This website is made with web app behavior and user interactivity in mind, thus focusing on the :green[User Interface] & :green[User Experience (UI/UX)] for elivated engagement."
+)
